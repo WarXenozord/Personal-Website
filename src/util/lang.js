3977a89978f9dff111c11d1c28279ce9b22ccafe
Juan Libonatti <libonatti@hotmail.com>
2025-04-27 16:23:58 -0300

    feat: automated language switching on render The app now checks user's browser language and updates the lang prop to portuguese or english

--- src/util/lang.js
+++ src/util/lang.js
@@ -6,12 +6,18 @@ export const LanguageContext = createContext({
 });
 
 export const useLang = () =>{
-    const [lang, setLang] = useState('en');
+    // Checking our user language and setting the website to it
+    const userLang = navigator.language || navigator.userLanguage;
+    const langCode = userLang.split('-')[0];
+    const [lang, setLang] = useState(langCode === 'pt'? 'pt':'en');
+    document.documentElement.lang = lang;
 
     const langMode = useMemo(
         () => ({
-            toggleLanguage: () => 
-                setLang((prev) => (prev === 'en' ? 'pt' : 'en')),
+            toggleLanguage: () => {
+                setLang((prev) => (prev === 'en' ? 'pt' : 'en'));
+                document.documentElement.lang = lang;
+             },
         }),
         []
     );
