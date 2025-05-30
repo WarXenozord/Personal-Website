a6dbc79f8d3e9f89d4a40466a094ad096a35b81f
Juan Libonatti <libonatti@hotmail.com>
2025-05-30 17:09:44 -0300

    performance: optimizing vite chunk division in build

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,16 +1,28 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': {
-        target: 'http://localhost:3001', // your Express server
+      "/api": {
+        target: "http://localhost:3001",
         changeOrigin: true,
         secure: false,
       },
     },
   },
-})
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          // Splits out React libs
+          react: ["react", "react-dom"],
+          // Split MUI out
+          mui: ["@mui/material", "@emotion/react", "@emotion/styled"],
+        },
+      },
+    },
+  },
+});
