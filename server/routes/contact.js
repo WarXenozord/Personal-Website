e1df8f9d6e4b8a4d010f88f2bd665c19285672da
Juan Libonatti <libonatti@hotmail.com>
2025-06-02 22:34:38 -0300

    feat: preparing for AWS SES support in contact form

--- server/routes/contact.js
+++ server/routes/contact.js
@@ -8,6 +8,35 @@ const contactLimiter = rateLimit({
   max: 2,
   message: { error: 'Too many submissions, please wait 10 minutes before sending another message.' },
 });
+const isProd = process.env.NODE_ENV === 'production';
+
+const sendWithGoogle = async ({ name, email, message }) => {
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: process.env.GMAIL_USER,
+      pass: process.env.GMAIL_APP_PASSWORD,
+    },      
+    tls: {
+      rejectUnauthorized: false,
+    },
+  });
+
+  return transporter.sendMail({
+      from: `"Website Contact" <no-reply@juanlibonatti.com>`,
+      to: process.env.GMAIL_USER,
+      subject: "New Contact Form Submission",
+      text: `
+        Name: ${name}
+        Email: ${email}
+        Message: ${message}
+      `,
+    });
+};
+
+const sendWithSES = async ({ name, email, message }) => {
+  console.log("to be done")
+};
 
 router.post("/contact", contactLimiter, async (req, res) => {
   const { name, email, message, honeypot } = req.body;
@@ -18,28 +47,11 @@ router.post("/contact", contactLimiter, async (req, res) => {
     return res.status(400).json({ error: "Missing fields" });
 
   try {
-    const transporter = nodemailer.createTransport({
-      service: "gmail",
-      auth: {
-        user: process.env.EMAIL,
-        pass: process.env.APP_PASSWORD,
-      },
-      //tls: {
-      //  rejectUnauthorized: false,
-      //},
-    });
+    if(isProd){
 
-    await transporter.sendMail({
-      from: `"Website Contact" <no-reply@juanlibonatti.com>`,
-      to: process.env.EMAIL,
-      subject: "New Contact Form Submission",
-      text: `
-        Name: ${name}
-        Email: ${email}
-        Message: ${message}
-      `,
-    });
-
+    }else{
+      await sendWithGoogle ({ name, email, message })
+    }
     res.status(200).json({ success: true });
   } catch (err) {
     console.error("Error sending mail:", err);
